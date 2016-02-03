ce4d10953cf92af3312aef74daab328316e2c1f1
Sebastian Abondano <sabondano1@gmail.com>
2016-02-02 22:10:25 -0500

    Extract explaining variables withing the total method of Tipper

--- lib/tipper.rb
+++ lib/tipper.rb
@@ -8,10 +8,23 @@ class Tipper
   end
 
   def total
-    amount + (amount * TAX) - (amount * (discount_percentage / 100.0)) + (amount * (tip_percentage / 100.0))
+    amount + tax - discount + tip
   end
 
   private
 
   attr_reader :amount, :discount_percentage, :tip_percentage
+
+  def tax
+    amount * TAX
+  end
+
+  def discount
+    amount * (discount_percentage / 100.0)
+  end
+
+  def tip
+    amount * (tip_percentage / 100.0)
+  end
+
 end
